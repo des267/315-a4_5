58a37486ffb7bf88223f740e85f43ad133e0fb2b
des267 <dstular@yahoo.com>
2024-02-08 20:02:56 -0700

    [DB] Create monster model

---
+++ models/monster.model.js
@@ -0,0 +1,38 @@
+/**
+ * monster.model.js
+ *
+ * Model object used by Mongoose
+ */
+
+import mongoose from "mongoose";
+
+const MonsterSchema = new mongoose.Schema(
+    {
+        "id": Number,
+        "name": String,
+        "username": String,
+        "email": String,
+        "address": {
+            "street": String,
+            "suite": String,
+            "city": String,
+            "zipcode": String,
+            "geo": {
+                "lat": Number,
+                "lng": Number
+            }
+        },
+        "phone": String,
+        "website": String,
+        "company": {
+            "name": String,
+            "catchPhrase": String,
+            "bs": String
+        },
+        "image_url": String
+    },
+    {timestamps: true, strictQuery: true}
+);
+
+const Monster = mongoose.model("Monster", MonsterSchema);
+export default Monster;
