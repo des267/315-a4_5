38cb28c88711bb36bc4c10bb3b9c7d36c764a776
Desmond Stular <dstular@yahoo.com>
2024-02-10 17:35:58 -0700

    set specific db to connect to

--- database/database.js
+++ database/database.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', true);
 export const connectDB = async () => {
 	const url = 'mongodb://localhost:27017/lab4';
 	try {
-		const connection = await mongoose.connect(process.env.DATABASE_URL || url);
+		const connection = await mongoose.connect(process.env.DATABASE_URL || url, {"dbName": "lab45"});
 		console.log("Database connected");
 	} catch (e) {
 		console.log("Could not connect");
