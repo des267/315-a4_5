dd2a31442fadc782c1199371929887d6b9c47da6
Desmond Stular <dstular@yahoo.com>
2024-02-10 17:19:08 -0700

    Added process environmental variable to database connect

--- database/database.js
+++ database/database.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', true);
 export const connectDB = async () => {
 	const url = 'mongodb://localhost:27017/lab4';
 	try {
-		const connection = await mongoose.connect(url);
+		const connection = await mongoose.connect(process.env.DATABASE_URL || url);
 		console.log("Database connected");
 	} catch (e) {
 		console.log("Could not connect");
