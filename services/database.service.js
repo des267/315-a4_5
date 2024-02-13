72de2c886dcf49c9d49e123963e022ff6c651975
Desmond Stular <dstular@yahoo.com>
2024-02-13 12:22:09 -0700

    added dbName as there was no connection

--- services/database.service.js
+++ services/database.service.js
@@ -11,7 +11,7 @@ mongoose.set('strictQuery', true);
 export const connectDB = async () => {
 	//const url = 'mongodb://localhost:27017/lab4';
 	try {
-		const connection = await mongoose.connect(process.env.DATABASE_URL);
+		const connection = await mongoose.connect(process.env.DATABASE_URL, {dbName: "lab45"});
 		console.log("Database connected");
 	} catch (e) {
 		console.log("Could not connect");
