0c9982d2cf778a03d7901136018536d3705ba1e6
Desmond Stular <dstular@yahoo.com>
2024-02-13 09:50:31 -0700

    Added home route to show server is online

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
-import router from "./routes/monsters.route.js";
+import home from "./routes/home.route.js";
 import monsters from "./routes/monsters.route.js";
-import { connectDB } from "./database/database.js";
+import { connectDB } from "./services/database.service.js";
 
 // create express server
 const app = express();
 const port = 3000;
 
-// Connect to database
+// Connect to services
 connectDB();
 
 // Setup json parser
@@ -16,6 +16,9 @@ app.use(bodyParser.json());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Use home route
+app.use("/", home);
 
 // Use monster routes
 app.use("/monsters", monsters);
--- services/database.service.js
+++ services/database.service.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 
 export const connectDB = async () => {
-	const url = 'mongodb://localhost:27017/lab4';
+	//const url = 'mongodb://localhost:27017/lab4';
 	try {
-		const connection = await mongoose.connect(process.env.DATABASE_URL || url, {"dbName": "lab45"});
+		const connection = await mongoose.connect(process.env.DATABASE_URL, {"dbName": "lab45"});
 		console.log("Database connected");
 	} catch (e) {
 		console.log("Could not connect");
